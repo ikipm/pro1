16c02eb1cf9b756c9588fea5e965e711107c3fc0
Iker Pérez <ikipm0@gmail.com>
2024-10-01 19:30:13 +0200

    Add: New Jutge D2_T1 exercise

---
+++ D2_T1/X76141.cc
@@ -0,0 +1,42 @@
+// Input: A positive integer, n > 0, asking for the first n “strange” numbers.
+// Output: Prints the list with the first n “strange” numbers.
+// Observation: It is forbidden to use any external function except cin and cout, and also the program must be well documented if not it will be invalidated.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int found = 0, n;
+    cin >> n; // Llegeix el número que indica quants nombres estranys volem trobar
+
+    for (int i = 1; found < n; i++) { // Bucle que recorre des del número 1 fins que trobi n nombres estranys, on a és el nombre d'estranys trobats
+        int temp = i, sum = 0, length = 0;
+
+        // Calcula el nombre de dígits de i
+        while (temp > 0) {
+            temp /= 10;
+            length++;
+        }
+
+        temp = i;
+        // Calcula la suma de les potències de cada dígit elevat a la longitud, sumant cada potència a sum. (Exemple: 153 = 1^3 + 5^3 + 3^3)
+        while (temp > 0) {
+            int digit = temp % 10;
+            int powPartial = 1;
+            // Calcula la potència parcial del dígit
+            for (int k = 0; k < length; k++) {
+                powPartial *= digit;
+            }
+            sum += powPartial; // Suma la potència parcial a la variable amb el resultat
+            temp /= 10;
+        }
+
+        // Si la suma de les potències dels dígits és igual a i, vol dir que i és un nombre estrany
+        if (sum == i) {
+            cout << i << " ";
+            found++;
+        }
+    }
+
+    return 0;
+}
